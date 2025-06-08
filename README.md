c6a7e7223ced481df3bb8b1a06c2f530219ce724
coryrylan <splintercode.cb@gmail.com>
2025-06-08 11:50:12 -0500

    init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # using-web-components-in-angular
 
 [Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/coryrylan/using-web-components-in-angular)
+
+https://coryrylan.com/blog/using-web-components-in-angular
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'web-component-essentials';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  <h1>Angular Application using Web Components</h1>
+
+  <p>
+    {{open ? 'open' : 'closed'}}
+  </p>
+  
+  <x-dropdown [title]="myTitle" (show)="toggle($event)">
+    Hello from Web Component in Angular!
+  </x-dropdown>
   `,
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA // Tells Angular we will have custom tags in our templates
+  ]
 })
 export class App {
-  name = 'Angular';
+  myTitle = 'project-angular';
+  open = false;
+
+  toggle(event: any) {
+    console.log(event);
+    this.open = event.detail;
+  }
 }
 
 bootstrapApplication(App);
